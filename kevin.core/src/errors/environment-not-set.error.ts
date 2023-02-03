48e8f4d5e35ac5902e3a58928c9992df2b657610
TKul6 <tomer.kh224@gmail.com>
2023-02-03 14:29:49 +0200

    another error

--- kevin.core/src/errors/environment-not-set.error.ts
+++ kevin.core/src/errors/environment-not-set.error.ts
@@ -2,4 +2,4 @@ export class EnvironmentNotSetError extends Error {
     constructor() {
         super("Environment is not set. Please set the environment using the setCurrentEnvironment method");
     }
-}
+}dvsfv
