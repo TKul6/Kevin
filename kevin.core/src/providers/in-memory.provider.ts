a31b998731a58eb7330aa01e9e3d10f94b469895
TKul6 <tomer.kh224@gmail.com>
2023-02-07 21:52:37 +0200

    Introduce in-memory-provider

---
+++ kevin.core/src/providers/in-memory.provider.ts
@@ -0,0 +1,26 @@
+import { IProvider } from "../interfaces";
+export class InMemoryProvider implements IProvider {
+
+    private store = new Map<string, string>();
+
+    getValue(key: string): Promise<string> {
+        return Promise.resolve(this.store.get(key));
+    }
+    setValue(key: string, value: string): Promise<void> {
+        this.store.set(key, value);
+        return Promise.resolve();
+    }
+    async getValueRange(keyPrefix: string): Promise<string[]> {
+        const keys = await this.getKeys(keyPrefix);
+
+        return keys.filter(key => key.startsWith(keyPrefix)).map(key => this.store.get(key));
+
+    }
+    getKeys(keyPrefix: string): Promise<string[]> {
+        return Promise.resolve(Array.from(this.store.keys()).filter(key => key.startsWith(keyPrefix)));
+    }
+    hasKey(key: string): Promise<boolean> {
+        return Promise.resolve(this.store.has(key));
+    }
+
+}
