e4e157158d3cd2aa47fffc63e88f8f46a0559fb1
Tomer Khalili <7823319+TKul6@users.noreply.github.com>
2023-03-11 10:48:17 +0200

    Kevin server (#25)
    * Introduce basic  http server for Kevin.
    
    * create server's controllers
    
    * support nodemon + build command
    
    * Adding githubActions + lint support
    
    * Fixing lint issues.
    
    * Fix action

---
+++ kevin.server/src/controllers/environments.controller.ts
@@ -0,0 +1,31 @@
+import {  type IEnvironmentInformation, type IEnvironmentMetaData, type IKevinManager } from '@kevin-infra/core/interfaces';
+import { Param, Body, Get, Post, JsonController } from 'routing-controllers';
+import {Service, Inject} from 'typedi';
+import { CreateEnvironmentModel } from '../models/create-environment.model';
+@JsonController("/environments")
+@Service()
+export class EnvironmentsController {
+
+    constructor(@Inject("kevin.service") private readonly kevinService: IKevinManager) {
+    }
+
+  @Get()
+  public async getEnvironments(): Promise<IEnvironmentMetaData[]> {
+    return await this.kevinService.getEnvironments();
+  }
+
+@Post()
+   async createRootEnvironment(): Promise<IEnvironmentInformation> {
+   
+    return await this.kevinService.createRootEnvironment();
+   }
+
+
+   @Post("/:id")
+   async createEnvironment(@Body() data: CreateEnvironmentModel, @Param("id") id: string): Promise<IEnvironmentMetaData> {
+   
+    return await this.kevinService.createEnvironment(data.environmentName, id);
+   }
+
+}
+
