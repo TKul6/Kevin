19f09097315f3c6c65f878f0a360801e271a9913
TKul6 <tomer.kh224@gmail.com>
2023-03-11 11:47:26 +0200

    w.i.p

--- kevin.server/src/index.ts
+++ kevin.server/src/index.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
-import { RedisProvider } from '@kevin-infra/redis';
+// import { RedisProvider } from '@kevin-infra/redis';
 import { KevinService } from '@kevin-infra/core/services';
 import { createExpressServer, useContainer } from 'routing-controllers';
 import { Container } from 'typedi';
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 import { EnvironmentKeysController } from './controllers/kv.controller';
 import { EnvironmentsController } from './controllers/environments.controller';
+import { InMemoryProvider } from '@kevin-infra/core/src/providers/in-memory.provider';
 
-Container.set('kevin.service', new KevinService(new RedisProvider(new Redis("redis://localhost:6379"))));
+// Container.set('kevin.service', new KevinService(new RedisProvider(new Redis("redis://localhost:6379"))));
+Container.set('kevin.service', new KevinService(new InMemoryProvider()));
 
 useContainer(Container);
 
