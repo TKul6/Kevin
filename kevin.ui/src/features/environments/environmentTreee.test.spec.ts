b482f378acfea93917fcbd9eb3797d685e2d28c0
TKul6 <tomer.kh224@gmail.com>
2023-04-26 12:01:43 +0300

    Fixing tests....

--- kevin.ui/src/features/environments/environmentTreee.test.spec.ts
+++ kevin.ui/src/features/environments/environmentTreee.test.spec.ts
@@ -1,5 +1,10 @@
 // TODO: this test should be deleted once normal tests exists.
 
-it("should pass", () => {
-    expect(true).toBe(true);
+describe("demo test", () => {
+
+    it("should pass", () => {
+        expect(true).toBe(true);
+    })
 })
+
+export {}
