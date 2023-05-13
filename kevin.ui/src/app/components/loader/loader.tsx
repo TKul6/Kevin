943e8a10672ec9a2ca7dd2c8bc68c7fdf1fd47ab
TKul6 <tomer.kh224@gmail.com>
2023-05-13 12:49:32 +0300

    Support loader in environment info.

--- kevin.ui/src/app/components/loader/loader.tsx
+++ kevin.ui/src/app/components/loader/loader.tsx
@@ -1,6 +1,7 @@
 import styles from "./loader.module.css";
 import { LoadingStatus } from "../../types";
 import { BallTriangle } from '@agney/react-loading';
+import { style } from "@mui/system";
 
 export interface LoaderInfo {
     status: LoadingStatus;
@@ -13,7 +14,7 @@ export interface LoaderInfo {
 }
 
 const DEFAULT_PROPS: Partial<LoaderInfo> = {
-    notLoadedMessage: "No Data",
+    notLoadedMessage: "Data is not loaded yet...",
     loadingMessage: "Loading",
     errorMessage: "Error",
     noDataMessage: "No Data"
@@ -26,26 +27,27 @@ export function Loader(props: LoaderInfo) {
 
     const componentProps = { ...DEFAULT_PROPS, ...props }
 
-
-switch (componentProps.status) {
-    case LoadingStatus.NotLoaded:
-        return (<div>{componentProps.notLoadedMessage}</div>);
-    case LoadingStatus.Loaded:
-        return (<div>{props.children}</div>);
-    case LoadingStatus.Failed:
-        return (<div className={styles.container}><div className={[styles.text, styles.error].join(" ")}>{componentProps.errorMessage}</div></div>);
-    case LoadingStatus.NoData:
-        return (<div>{componentProps.noDataMessage}</div>);
-    case LoadingStatus.Loading:
-        return (<div className={styles.container}>
-            <div className={styles.loaderContainer}>
-            <BallTriangle />
-            </div>
-            <div className={styles.text}>{componentProps.loadingMessage}</div></div>);
-}
+    let inner = <div>Unhandled error</div>;
+    switch (componentProps.status) {
+        case LoadingStatus.NotLoaded:
+            inner = (<div className={styles.text}>{componentProps.notLoadedMessage}</div>);
+            break;
+        case LoadingStatus.Loaded:
+            return (<div>{props.children}</div>);
+        case LoadingStatus.Failed:
+            inner = (<div className={[styles.text, styles.error].join(" ")}>{componentProps.errorMessage}</div>);
+            break;
+        case LoadingStatus.NoData:
+            inner = (<div className={styles.text}>{componentProps.noDataMessage}</div>);
+            break;
+        case LoadingStatus.Loading:
+            inner = (<div className={styles.container}><div className={styles.loaderContainer}>
+                <BallTriangle /></div> 
+                <div className={styles.text}>{componentProps.loadingMessage}</div></div>);
+            break;
+    }
 
     return (
-        <div>Unhandled error
-        </div>
+        <div className={styles.container}>{inner}</div>
     )
 }
