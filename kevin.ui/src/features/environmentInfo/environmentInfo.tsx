c53c21740813d10aa72bd9d9d089b4ab5a9a874b
Tomer Khalili <7823319+TKul6@users.noreply.github.com>
2023-07-02 13:13:15 +0300

    Support filtering keys in the UI (#60)
    * Support filtering in environmentInfo
    
    * Fixing total length

--- kevin.ui/src/features/environmentInfo/environmentInfo.tsx
+++ kevin.ui/src/features/environmentInfo/environmentInfo.tsx
@@ -1,5 +1,4 @@
-
-import {  useAppSelector } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import styles from './environmentInfo.module.css';
 import Divider from '@mui/material/Divider';
 import Table from '@mui/material/Table';
@@ -10,7 +9,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import EditIcon from '@mui/icons-material/Edit';
-import { openAddKeyDialog, openInheritKeyDialog, openSetKeyValueDialog, selectEnvironmentInfo, selectLoadingStatus} from './state';
+import { openAddKeyDialog, openInheritKeyDialog, openSetKeyValueDialog, selectEnvironmentInfo, selectLoadingStatus } from './state';
 import { IKevinValue } from '@kevin-infra/core/interfaces';
 import { Header } from '../../app/components/header/header';
 import { AddKeyDialog } from './dialogs/addKeyDialog';
@@ -25,105 +24,130 @@ import React from 'react';
 import { IconCommand } from '../../app/components/icon-command/icon-command';
 import { InheritKeyDialog } from './dialogs/inheritKeyDialog';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-
-
-
+import TextField from '@mui/material/TextField';
 
 export function EnvironmentInfo() {
-
   const environmentInfo = useAppSelector(selectEnvironmentInfo);
   const status = useAppSelector(selectLoadingStatus);
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
+  const [rowsPerPage, setRowsPerPage] = React.useState(14);
+  const [keyFilterValue, setKeyFilterValue] = React.useState('');
+  const [valueFilterValue, setValueFilterValue] = React.useState('');
 
   function handleChangeRowsPerPage(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void {
     setRowsPerPage(parseInt(event.target.value));
     setPage(0);
-    
   }
 
   function handleChangePage(event: any, page: number): void {
     setPage(page);
+  }
 
+  function getFilteredKeys() {
+    return environmentInfo.environmentKeys
+      .filter((kv: IKevinValue) => keyFilterValue === '' || kv.key.includes(keyFilterValue))
+      .filter((kv: IKevinValue) => valueFilterValue === '' || kv.value.includes(valueFilterValue));
   }
 
   return (
     <div className={styles.contentContainer}>
-      <Header title='Environment Info' commands={[{ name: 'Create', tooltip: 'Create new key', action: openAddKeyDialog(), hidden: status !== LoadingStatus.Loaded }]} />
+      <Header
+        title="Environment Info"
+        commands={[
+          {
+            name: 'Create',
+            tooltip: 'Create new key',
+            action: openAddKeyDialog(),
+            hidden: status !== LoadingStatus.Loaded,
+          },
+        ]}
+      />
       <Divider></Divider>
       <div className={styles.item}>
-
-<Loader status={status} loadingMessage="Loading environment keys ... Don't worry, they're out there!" errorMessage="Failed to load Environment Keys"
-notLoadedMessage="Select an environment to revel it's keys">
-        <TableContainer component={Paper}>
-          <Table aria-label="Keys table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Key</TableCell>
-                <TableCell>Value</TableCell>
-                <TableCell className={styles.actionsColumn}></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {(rowsPerPage > 0 ? environmentInfo.environmentKeys.slice(rowsPerPage * page,rowsPerPage * page + rowsPerPage): environmentInfo.environmentKeys.slice(0, rowsPerPage)).map((kvInfo: IKevinValue) => (
-                <TableRow key={kvInfo.key}>
-                  <TableCell className={kvInfo.environmentInfo.id !== environmentInfo.selectedEnvironmentId ? styles.inheritItem : ''}>{kvInfo.key}</TableCell>
-                  <TableCell className={kvInfo.environmentInfo.id !== environmentInfo.selectedEnvironmentId ? styles.inheritItem : ''}>{kvInfo.value}</TableCell>
-                  <TableCell className={styles.actionsColumn} align="right">
-
-                  <IconCommand tooltip={kvInfo.environmentInfo.id !== environmentInfo.selectedEnvironmentId ? 'Stop inherit and set value' : 'Set value'}
-                    tooltipPlacement="top"
-                    ariaLabel="set value"
-                    fontSize="small"
-                    action={openSetKeyValueDialog(kvInfo)}>
-                       <EditIcon fontSize="small" />
-                    </IconCommand>
-
-                  {environmentInfo.selectedEnvironmentId !== "root" &&  kvInfo.environmentInfo.id === environmentInfo.selectedEnvironmentId && (
-                  <IconCommand tooltip="Inherit value from parent environment"
-                    tooltipPlacement="top"
-                    ariaLabel="inherit value"
-                    fontSize="small"
-                    action={openInheritKeyDialog(kvInfo)}>
-                       <ArrowUpwardIcon fontSize="small" />
-                    </IconCommand>)}
-                  </TableCell>
+        <Loader
+          status={status}
+          loadingMessage="Loading environment keys ... Don't worry, they're out there!"
+          errorMessage="Failed to load Environment Keys"
+          notLoadedMessage="Select an environment to revel it's keys"
+        >
+          <TableContainer component={Paper}>
+            <Table aria-label="Keys table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Key</TableCell>
+                  <TableCell>Value</TableCell>
+                  <TableCell className={styles.actionsColumn}></TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-      <TableFooter>
-          <TableRow>
-                    <TablePagination
-              colSpan={3}
-              rowsPerPageOptions={[5, 10]}
-              count={environmentInfo.environmentKeys.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-          </Table>
-        </TableContainer>
-        <SetKeyDialog />
-      <AddKeyDialog />
-      <InheritKeyDialog />
+                <TableRow>
+                  <TableCell>
+                    <TextField fullWidth id="standard-basic" label="Filter" variant="standard" onChange={(e) => setKeyFilterValue(e.target.value)} />
+                  </TableCell>
+                  <TableCell>
+                    <TextField fullWidth className="flex-grow" id="standard-basic" label="Filter" variant="standard" onChange={(e) => setValueFilterValue(e.target.value)} />
+                  </TableCell>
+                  <TableCell className={styles.actionsColumn}></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(rowsPerPage > 0 ? getFilteredKeys().slice(rowsPerPage * page, rowsPerPage * page + rowsPerPage) : getFilteredKeys().slice(0, rowsPerPage)).map(
+                  (kvInfo: IKevinValue) => (
+                    <TableRow key={kvInfo.key}>
+                      <TableCell className={kvInfo.environmentInfo.id !== environmentInfo.selectedEnvironmentId ? styles.inheritItem : ''}>{kvInfo.key}</TableCell>
+                      <TableCell className={kvInfo.environmentInfo.id !== environmentInfo.selectedEnvironmentId ? styles.inheritItem : ''}>{kvInfo.value}</TableCell>
+                      <TableCell className={styles.actionsColumn} align="right">
+                        <IconCommand
+                          tooltip={kvInfo.environmentInfo.id !== environmentInfo.selectedEnvironmentId ? 'Stop inherit and set value' : 'Set value'}
+                          tooltipPlacement="top"
+                          ariaLabel="set value"
+                          fontSize="small"
+                          action={openSetKeyValueDialog(kvInfo)}
+                        >
+                          <EditIcon fontSize="small" />
+                        </IconCommand>
+
+                        {environmentInfo.selectedEnvironmentId !== 'root' && kvInfo.environmentInfo.id === environmentInfo.selectedEnvironmentId && (
+                          <IconCommand
+                            tooltip="Inherit value from parent environment"
+                            tooltipPlacement="top"
+                            ariaLabel="inherit value"
+                            fontSize="small"
+                            action={openInheritKeyDialog(kvInfo)}
+                          >
+                            <ArrowUpwardIcon fontSize="small" />
+                          </IconCommand>
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  )
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    colSpan={3}
+                    rowsPerPageOptions={[5, 10, 14]}
+                    count={getFilteredKeys().length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    SelectProps={{
+                      inputProps: {
+                        'aria-label': 'rows per page',
+                      },
+                      native: true,
+                    }}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationActions}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+          <SetKeyDialog />
+          <AddKeyDialog />
+          <InheritKeyDialog />
         </Loader>
       </div>
-      
     </div>
-
-
   );
-
 }
-
